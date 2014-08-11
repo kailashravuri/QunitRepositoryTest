9ad7dc7857cef66fcf8eefc8a5874e83352c1ef0
kailashravuri <kailu.ravuri@gmail.com>
2014-08-11 12:17:09 +0530

    Changes to method name

---
+++ QUnitTestApplication/QunitcodeTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QunitcodeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void simpleTest()
+        {
+            int testValue = 5;
+            Assert.Equals(testValue, 5);
+        }
+    }
+}
